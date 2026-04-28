0af7e4960634e55886acb13f4e2fda26449dc284
Cheejyg <glenncheejunyuan@hotmail.com>
2026-04-29 00:14:51 +0800

    Add Sudoku display_grid()

--- app/sudoku.py
+++ app/sudoku.py
@@ -8,3 +8,16 @@ class Sudoku:
         self.pre_filled_numbers = pre_filled_numbers
         self.empty_cells_representation = empty_cells_representation
         self.grid = [[None for _ in range(9)] for _ in range(9)]
+
+    def display_grid(self) -> None:
+        output = "    " + " ".join(map(str, [i + 1 for i in range(0, 9)])) + "\n"
+
+        j = ord("A")
+        for row in range(len(self.grid)):
+            output += "  " + chr(j)
+            for col in range(len(self.grid[row])):
+                output += " " + (str(self.grid[row][col]) if self.grid[row][col] else self.empty_cells_representation)
+            output += "\n"
+            j += 1
+
+        print(output.rstrip())
