e45f8a1d22bf78d1d662af6e424b3f28e1ca291b
Cheejyg <glenncheejunyuan@hotmail.com>
2026-04-29 02:10:00 +0800

    Add `tests` generate_puzzle

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -9,3 +9,16 @@ def test_init():
         pre_filled_numbers=pre_filled_numbers,
     )
     assert sudoku.pre_filled_numbers == pre_filled_numbers
+
+
+def test_generate_puzzle():
+    pre_filled_numbers = random.randint(0, (9 * 9) - 1)
+    sudoku = Sudoku(
+        pre_filled_numbers=pre_filled_numbers,
+    )
+    filled_numbers = 0
+    for row in range(len(sudoku.grid)):
+        for col in range(len(sudoku.grid[row])):
+            if sudoku.grid[row][col]:
+                filled_numbers += 1
+    assert pre_filled_numbers == filled_numbers
