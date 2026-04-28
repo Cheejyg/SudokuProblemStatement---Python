f6e80f1c4d2c28113e51ac87719bb0b76d2f0619
Cheejyg <glenncheejunyuan@hotmail.com>
2026-04-29 00:50:25 +0800

    Add Sudoku generate_puzzle()

--- app/sudoku.py
+++ app/sudoku.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Sudoku:
     def __init__(
         self,
@@ -7,7 +10,27 @@ class Sudoku:
     ) -> None:
         self.pre_filled_numbers = pre_filled_numbers
         self.empty_cells_representation = empty_cells_representation
-        self.grid = [[None for _ in range(9)] for _ in range(9)]
+        self.grid: list[list[int | None]] = [[None for _ in range(9)] for _ in range(9)]
+
+        def generate_number(low: int, high: int) -> int:
+            return random.randint(low, high)
+
+        def generate_puzzle() -> None:
+            self.pre_filled_cells = set()
+
+            for i in range(pre_filled_numbers):
+                number = generate_number(1, 9)
+                row = generate_number(0, 8)
+                col = generate_number(0, 8)
+                while (row, col) in self.pre_filled_cells:
+                    number = generate_number(0, 8)
+                    row = generate_number(0, 8)
+                    col = generate_number(0, 8)
+                # TODO: check_validity
+                self.grid[row][col] = number
+                self.pre_filled_cells.add((row, col))
+
+        generate_puzzle()
 
     def display_grid(self) -> None:
         output = "    " + " ".join(map(str, [i + 1 for i in range(0, 9)])) + "\n"
