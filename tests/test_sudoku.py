97df56699f32f301780dcf1f8c2d74d65afb390a
Cheejyg <glenncheejunyuan@hotmail.com>
2026-04-29 02:02:58 +0800

    Add `tests` init

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,2 +1,11 @@
-def test_main():
-    pass
+import random
+
+from app.sudoku import Sudoku
+
+
+def test_init():
+    pre_filled_numbers = random.randint(0, (9 * 9) - 1)
+    sudoku = Sudoku(
+        pre_filled_numbers=pre_filled_numbers,
+    )
+    assert sudoku.pre_filled_numbers == pre_filled_numbers
