79ebcb4f13b27031a8ff3e9196d9685c70e9dbbd
Cheejyg <glenncheejunyuan@hotmail.com>
2026-04-28 23:58:36 +0800

    Add Sudoku __init__(...)

--- app/sudoku.py
+++ app/sudoku.py
@@ -1,3 +1,10 @@
 class Sudoku:
-    def __init__(self,) -> None:
-        pass
+    def __init__(
+        self,
+        *,
+        pre_filled_numbers: int = 30,
+        empty_cells_representation: str = "_",
+    ) -> None:
+        self.pre_filled_numbers = pre_filled_numbers
+        self.empty_cells_representation = empty_cells_representation
+        self.grid = [[None for _ in range(9)] for _ in range(9)]
